b07e70c0f18129c9b4746e2058bdf0fc1d0bde65
freetimezp <kraftukrnet@ukr.net>
2023-05-04 18:17:35 +0300

    05. js-social-09-images. FollowersCard component.

---
+++ src/Data/FollowersData.js
@@ -0,0 +1,27 @@
+import img1 from '../img/img1.png';
+import img2 from '../img/img2.png';
+import img3 from '../img/img3.png';
+import img4 from '../img/img4.jpg';
+
+export const Followers = [
+    {
+        name: "Andrew Thomas",
+        username: "AndrewThomas",
+        img: img1,
+    },
+    {
+        name: "Second Thomas",
+        username: "SecondThomas",
+        img: img2,
+    },
+    {
+        name: "Third Thomas",
+        username: "ThirdThomas",
+        img: img3,
+    },
+    {
+        name: "Four Thomas",
+        username: "FourThomas",
+        img: img4,
+    },
+];
