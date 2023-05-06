6e1593f24997d32cea47f15f98046d520f1e9b3a
freetimezp <kraftukrnet@ukr.net>
2023-05-06 19:22:05 +0300

    13. js-social-09-images. User model.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import AuthRoute from './Routes/AuthRoute.js';
 
 import * as dotenv from 'dotenv';
 dotenv.config();
@@ -9,12 +10,13 @@ const user = process.env.NAME_MONGO_USER;
 const pass = process.env.PASS_MONGO_USER;
 const port = process.env.PORT_MONGO_SERVER;
 
+//server
 const app = express();
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
+//connect to Mong db
 const query = "mongodb+srv://" + user + ":" + pass + "@cluster0.hi4nuw5.mongodb.net/SocialMedia?retryWrites=true&w=majority";
-
 mongoose.connect(
     query, { useNewUrlParser: true, useUnifiedTopology: true }
 ).then(
@@ -22,3 +24,6 @@ mongoose.connect(
 ).catch((error) => {
     console.log(error);
 });
+
+//routes
+app.use('/auth', AuthRoute);
