c3cde40abbac1294ca8806e7a1e494354b122f23
freetimezp <kraftukrnet@ukr.net>
2023-05-09 21:56:52 +0300

    25. js-social-09-images. Like, unlike post.

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import './Post.css';
@@ -7,21 +7,34 @@ import Comment from '../../img/comment.png';
 import Share from '../../img/share.png';
 import Heart from '../../img/like.png';
 import NotLike from '../../img/notlike.png';
+import { likePost } from '../../api/PostRequest';
 
 const Post = ({ data }) => {
     const { user } = useSelector((state) => state.AuthReducer.authData);
+    const [liked, setLiked] = useState(data.likes.includes(user._id));
+    const [likes, setLikes] = useState(data.likes.length);
+
+    const handleLike = () => {
+        setLiked((prev) => !prev);
+        likePost(data._id, user._id);
+        liked ? setLikes((prev) => prev - 1) : setLikes((prev) => prev + 1);
+    };
 
     return (
         <div className='post'>
             <img src={data.image ? process.env.REACT_APP_PUBLIC_FOLDER + data.image : ""} alt="user" className='main-img' />
 
             <div className="postReact">
-                <img src={data.liked ? Heart : NotLike} alt="icon" />
+                <img
+                    src={liked ? Heart : NotLike}
+                    alt="icon"
+                    onClick={handleLike}
+                />
                 <img src={Comment} alt="icon" />
                 <img src={Share} alt="icon" />
             </div>
 
-            <span style={{ color: "var(--gray)", fontSize: "12px" }}>{data.likes} likes</span>
+            <span style={{ color: "var(--gray)", fontSize: "12px" }}>{likes} likes</span>
 
             <div className="detail">
                 <span><b>{data.name}</b></span>
